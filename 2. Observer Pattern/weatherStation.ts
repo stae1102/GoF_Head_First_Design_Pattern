657eaaf44686f0a4afd6b072ce69f9ae54be11b2
Seongtae <stae1102@gmail.com>
2023-07-25 17:46:01 +0900

    feat: 기상 스테이션 구현을 위한 인터페이스 추가

---
+++ 2. Observer Pattern/weatherStation.ts
@@ -0,0 +1,15 @@
+export interface Subject {
+  registerObserver: (observer: Observer) => void; // 옵저버 등록
+  removeObserver: (observer: Observer) => void; // 옵저버 삭제
+  notifyObserver: () => void; // 주제의 상태가 변경되었을 때, 모든 옵저버에게 변경 내용을 알림
+}
+
+// 옵저버 interface는 옵저버 클래스에서 구현해야 하며 즉, update 메서드를 구현해야 한다.
+export interface Observer {
+  update: (temp: number, humidity: number, pressure: number) => void;
+}
+
+// 디스플레이 항목을 화면에 표시하기 위함
+export interface DisplayElement {
+  display: () => void;
+}
