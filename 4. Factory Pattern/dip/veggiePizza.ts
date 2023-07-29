fc75334e1a13acd9d659de7456d33dd727d87d51
Seongtae <stae1102@gmail.com>
2023-07-29 22:34:57 +0900

    feat: 나머지 피자 객체 추가

---
+++ 4. Factory Pattern/dip/veggiePizza.ts
@@ -0,0 +1,19 @@
+import { Pizza } from './pizza';
+import { PizzaIngredientFactory } from './pizzaIngredientFactory';
+
+export class VeggiePizza extends Pizza {
+  ingredientFactory: PizzaIngredientFactory;
+
+  constructor(ingredientFactory: PizzaIngredientFactory) {
+    super();
+    this.ingredientFactory = ingredientFactory;
+  }
+
+  prepare(): void {
+    console.log(`준비 중: ${this.name}`);
+    this.dough = this.ingredientFactory.createDough();
+    this.sauce = this.ingredientFactory.createSauce();
+    this.cheese = this.ingredientFactory.createCheese();
+    this.veggies = this.ingredientFactory.createVeggies();
+  }
+}
