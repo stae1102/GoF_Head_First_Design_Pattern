d702e3183ad642d287958c6b712bcfeb25c2fabe
Seongtae <stae1102@gmail.com>
2023-08-01 16:05:49 +0900

    feat: 차고 커맨드 객체를 인보커에 전달해 차고 열기 행동을 리시버로 호출

--- 6. Command Pattern/remoteControlTest.ts
+++ 6. Command Pattern/remoteControlTest.ts
@@ -1,10 +1,16 @@
+import { GarageDoor } from './garageDoor';
+import { GarageDoorOpenCommand } from './garageDoorOpenCommand';
 import { Light } from './light';
 import { LightOnCommand } from './lightOnCommand';
 import { SimpleRemoteControl } from './simpleRemoteControl';
 
-const remote: SimpleRemoteControl = new SimpleRemoteControl(); // 인보커
-const light: Light = new Light(); // 리시버
-const lightOn = new LightOnCommand(light); // 커맨드 객체
+const remote: SimpleRemoteControl = new SimpleRemoteControl(); // 리모컨 인보커
+const light: Light = new Light(); // 조명 리시버
+const garageDoor: GarageDoor = new GarageDoor(); // 차고 리시버
+const lightOn = new LightOnCommand(light); // 조명 커맨드 객체
+const garageOpen: GarageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor); // 차고 커맨드 객체
 
-remote.setCommand(lightOn); // 커맨드 객체를 인보커에 전달
-remote.buttonWasPressed(); // 리시버에 있는 행동 메서드 호출
+remote.setCommand(lightOn); // 조명 커맨드 객체를 인보커에 전달
+remote.buttonWasPressed(); // 리시버에 있는 조명 켜는 행동 메서드 호출
+remote.setCommand(garageOpen); // 차고 커맨드 객체를 인보커에 전달
+remote.buttonWasPressed(); // 리시버에 있는 차고 여는 행동 메서드 호출
