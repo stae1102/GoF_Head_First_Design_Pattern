af0d820189fb65192bc9f4cef925eb58718e62c7
Seongtae <stae1102@gmail.com>
2023-08-01 17:40:50 +0900

    feat: 리시버 위치 추가

--- 6. Command Pattern/garageDoor.ts
+++ 6. Command Pattern/garageDoor.ts
@@ -1,23 +1,27 @@
 export class GarageDoor {
-  public constructor() {}
+  location: string;
+
+  public constructor(location: string) {
+    this.location = location;
+  }
 
   public up(): void {
-    console.log('차고 문이 열렸습니다.');
+    console.log(`${this.location} 차고 문이 열렸습니다.`);
   }
 
   public down(): void {
-    console.log('차고 문이 닫혔습니다.');
+    console.log(`${this.location} 차고 문이 닫혔습니다.`);
   }
 
   public stop(): void {
-    console.log('차고 문이 멈췄습니다.');
+    console.log(`${this.location} 차고 문이 멈췄습니다.`);
   }
 
   public lightOn(): void {
-    console.log('차고 불이 켜졌습니다.');
+    console.log(`${this.location} 차고 불이 켜졌습니다.`);
   }
 
   public lightOff(): void {
-    console.log('차고 불이 꺼졌습니다.');
+    console.log(`${this.location} 차고 불이 꺼졌습니다.`);
   }
 }
--- 6. Command Pattern/light.ts
+++ 6. Command Pattern/light.ts
@@ -1,11 +1,15 @@
 export class Light {
-  constructor() {}
+  location: string;
+
+  constructor(location: string) {
+    this.location = location;
+  }
 
   public on(): void {
-    console.log('조명이 켜졌습니다.');
+    console.log(`${this.location} 조명이 켜졌습니다.`);
   }
 
   public off(): void {
-    console.log('조명이 꺼졌습니다.');
+    console.log(`${this.location} 조명이 꺼졌습니다.`);
   }
 }
