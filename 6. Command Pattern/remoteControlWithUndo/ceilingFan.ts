0ceb3b1c1334147c47a52c4a7045160acb0f52d4
Seongtae <stae1102@gmail.com>
2023-08-01 18:51:45 +0900

    feat: 선풍기 객체 추가, 선풍기 속도 high 설정 커맨드 객체 추가

---
+++ 6. Command Pattern/remoteControlWithUndo/ceilingFan.ts
@@ -0,0 +1,42 @@
+export class CeilingFan {
+  public static HIGH: number = 3;
+  public static MEDIUM: number = 2;
+  public static LOW: number = 1;
+  public static OFF: number = 0;
+  location: string;
+  speed: number;
+
+  public constructor(location: string) {
+    this.location = location;
+  }
+
+  public high(): void {
+    this.speed = CeilingFan.HIGH;
+    console.log(
+      `${this.location} 선풍기의 속도가 ${this.speed}로 설정되었습니다.`
+    );
+  }
+
+  public medium(): void {
+    this.speed = CeilingFan.MEDIUM;
+    console.log(
+      `${this.location} 선풍기의 속도가 ${this.speed}로 설정되었습니다.`
+    );
+  }
+
+  public low(): void {
+    this.speed = CeilingFan.LOW;
+    console.log(
+      `${this.location} 선풍기의 속도가 ${this.speed}로 설정되었습니다.`
+    );
+  }
+
+  public off(): void {
+    this.speed = CeilingFan.OFF;
+    console.log(`${this.location} 선풍기가 꺼졌습니다.`);
+  }
+
+  public getSpeed(): number {
+    return this.speed;
+  }
+}
